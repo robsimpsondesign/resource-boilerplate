17582e2647b4c63ad330a2dff8fde646ee38e00d
Rob Simpson <hello@robsimpson.me>
2018-04-14 09:48:03 +0100

    Updated project after creating a resource project
    Updated HTML and CSS compiled formatting in gulp file
    Updated package name and repo url
    Removed fonts folder and included direct link to google fonts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,7 +29,8 @@ gulp.task('html', () => {
 	gulp.src([`${srcpaths.html}/*.html`])
 		.pipe(include({
 			prefix: '@',
-			basepath: srcpaths.html
+			basepath: srcpaths.html,
+			indent: true
 		}))
 		.pipe(gulp.dest(distpaths.html));
 });
@@ -37,7 +38,11 @@ gulp.task('html', () => {
 // Compile SASS to CSS
 gulp.task('css', () => {
     gulp.src(`${srcpaths.css}/**/*.scss`)
-        .pipe(sass())
+        .pipe(sass({
+			indentType: 'tab',
+			indentWidth: 1,
+			outputStyle: 'expanded'
+		}))
         .pipe(autoprefixer({browsers: ['ie >= 11', 'ie_mob >= 11', 'ff >= 30', 'chrome >= 34', 'safari >= 7', 'opera >= 23', 'ios >= 7', 'android >= 4.4', 'bb >= 10']}))
 		.pipe(gulp.dest(distpaths.css));
 });
@@ -52,6 +57,6 @@ gulp.task('server', () => {
   });
 
 gulp.task('default', ['html', 'css', 'server'], () => {
-	gulp.watch(`${srcpaths.html}/*.html`, ['html']);
+	gulp.watch(`${srcpaths.html}/**/**/*.html`, ['html']);
 	gulp.watch(`${srcpaths.css}/**/*.scss`, ['css']);
 });
